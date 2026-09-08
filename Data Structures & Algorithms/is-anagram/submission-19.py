6ec32aa11b821525bdaf207557f0628a9681dc83
Soyzalo08 <gonzalofr2008@gmail.com>
2026-09-08 21:19:42 +0200

    Add: is-anagram - submission-19

---
+++ Data Structures & Algorithms/is-anagram/submission-19.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        count_t = {}
+        count_s = {}
+        for letter in s:
+            if letter not in count_s:
+                count_s[letter] = 1
+            else:
+                count_s[letter] += 1
+        
+        for letter in t:
+            if letter not in count_t:
+                count_t[letter] = 1
+            else:
+                count_t[letter] += 1
+        if count_t == count_s:
+            return True
+        return False
